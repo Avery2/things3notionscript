476947eb39305d3de6158dd6440090f6d990d5b3
Avery2 <justinaverychan@gmail.com>
2022-11-20 16:31:41 -0800

    docs: add howto on checklist

--- empty_things3_inbox_to_notion.py
+++ empty_things3_inbox_to_notion.py
@@ -66,10 +66,37 @@ if __name__ == "__main__":
     #     'start_date': None,
     #     'deadline': None,
     #     'stop_date': None,
+    #     'checklist': True,
     #     'created': '2022-11-20 00:02:54',
     #     'modified': '2022-11-20 16:22:42',
     #     'index': -50541,
     #     'today_index': 0
+    # }
+    # >>> import things
+    # >>> things.todos()
+    # [{'uuid': '2Ukg8I2nLukhyEM7wYiBeb',
+    # 'type': 'to-do',
+    # 'title': 'Make reservation for dinner',
+    # 'project': 'bNj6TPdKYhY6fScvXWVRDX',
+    # ...},
+    # {'uuid': 'RLZroza3jz0XPs3uAlynS7',
+    # 'type': 'to-do',
+    # 'title': 'Buy a whiteboard and accessories',
+    # 'project': 'w8oSP1HjWstPin8RMaJOtB',
+    # 'notes': "Something around 4' x 3' that's free-standing, two-sided, and magnetic.",
+    # 'checklist': True,
+    # ...
+    # >>> things.todos('RLZroza3jz0XPs3uAlynS7')
+    # {'uuid': 'RLZroza3jz0XPs3uAlynS7',
+    # 'type': 'to-do',
+    # 'title': 'Buy a whiteboard and accessories',
+    # ...
+    # 'checklist': [
+    #     {'title': 'Cleaning Spray', 'status': 'completed', ...},
+    #     {'title': 'Magnetic Eraser', 'status': 'incomplete', ...},
+    #     {'title': 'Round magnets', 'status': 'incomplete', ...}
+    # ]
+    # ...
     # }
 
     query = None
